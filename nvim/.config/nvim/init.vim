nick-cd/dotfiles

commit 5b848404f25aedee2e1462a806a04677aef4cc44

Added The use of the built-in spell checker
This commit now uses the built-in spell checker for neovim. The function
wrapper ensures that it does not run for specific files such as help and
the man filetypes.

nvim/.config/nvim/init.vim
@@ -47,7 +47,19 @@ fun! StripSpace()
 endfun
 autocmd BufEnter * call StripSpace()
 
-autocmd FileType sh,zsh,bash,fish setlocal shiftwidth=4 softtabstop=4 expandtab
+" Using the Built-in spell checker
+" This function blacklists files that I don't want spell checking on
+fun! SpellCheck()
+	let l:ext = expand("%:e")
+	" I programmed in these languages at one point ... don't judge!
+	" Spell check in them is very annoying
+	if l:ext =~ '.*rpgle$' || l:ext =~ 'clle' || l:ext =~ '.*pf$' || l:ext =~ 'lf' || &ft == 'help' || &ft == 'man' || &ft == 'gitrebase'
+		return
+	else
+		setlocal spell spelllang=en_ca
+	endif
+endfun
+autocmd VimEnter,BufReadPost * call SpellCheck()
 
 " vim-plug as the plug-in manager https://github.com/junegunn/vim-plug
 

commit 45e84100d5ca8a17342c25426190bb70995b1609

Fixed spell check activation bug
This bug was introduced in commit: 5b84840. This commit added logic to
ensure that spell checking will only activate when it makes
sense. However, it would still activate when a disallowed file type is
edited for the first time. This commit fixes the issue.

nvim/.config/nvim/init.vim
@@ -63,7 +63,7 @@ fun! SpellCheck()
 		setlocal spell spelllang=en_ca
 	endif
 endfun
-autocmd VimEnter,BufReadPost * call SpellCheck()
+autocmd VimEnter,BufEnter * call SpellCheck()
 
 " vim-plug as the plug-in manager https://github.com/junegunn/vim-plug
 